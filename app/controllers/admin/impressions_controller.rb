c1411829d04ebaa850a2f9aad7b3a3421f8694d3
EC2 Default User <ec2-user@ip-172-31-17-159.ap-northeast-1.compute.internal>
2023-02-06 15:59:43 +0000

    [管理者機能の作成]

--- app/controllers/admin/impressions_controller.rb
+++ app/controllers/admin/impressions_controller.rb
@@ -1,10 +1,16 @@
 class Admin::ImpressionsController < ApplicationController
   def index
+    @impressions = Impression.all
   end
 
   def show
+    @impression = Impression.find(params[:id])  
   end
 
   def destroy
+    impression = Impression.find(params[:id])  # データ（レコード）を1件取得
+    impression.destroy  # データ（レコード）を削除
+    redirect_to impressions_path  # 投稿一覧画面へリダイレクト  
   end
+  
 end
